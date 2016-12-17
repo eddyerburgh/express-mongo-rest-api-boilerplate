4d8ba8bcb1a82c9e9e682221fef81440772390df
Edd Yerburgh <edward.yerburgh@gmail.com>
2016-12-17 11:08:46 +0000

    Add cors support

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = require('./router');
 const errorHandler = require('../routes/handlers/errorHandler');
 const notFoundHandler = require('../routes/handlers/notFoundHandler');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/', router);
 
