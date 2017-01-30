157b5b3578070f87226f23d74ce7a86a6373557e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-30 06:53:18 +0000

    Connect to mongo with mongoose

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const db = require('../lib/db');
 const logger = require('../lib/logger');
 
 const PORT = 8080;
@@ -6,4 +7,18 @@ const PORT = 8080;
 app.listen(PORT, () => {
   logger.log('info', `Server listening on port ${PORT}!`);
   logger.log('info', 'Press CTRL-C to stop\n');
+});
+
+db.connection.on('error', (err) => {
+  logger.log('error', `connection error:${err}`);
+});
+
+db.connection.once('open', () => {
+  app.listen(PORT, (err) => {
+    if (err) {
+      logger.log('error', `Error starting server: ${err}`);
+      return;
+    }
+    logger.log('info', `server listening on ${PORT}`);
+  });
 });
